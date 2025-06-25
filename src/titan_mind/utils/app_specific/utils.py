2f369f115b4ca83540a80e96b88fdf3b8d10033b
shubh-SB <shubhashish@salarybox.in>
2025-06-25 16:44:52 +0530

    FIX

--- src/titan_mind/utils/app_specific/utils.py
+++ src/titan_mind/utils/app_specific/utils.py
@@ -5,5 +5,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def is_the_mcp_to_run_in_server_mode_or_std_dio():
-    run_in_remote_server_mode = os.getenv("RUN_REMOTE_SERVER_MODE", "True")
+    run_in_remote_server_mode = os.getenv("RUN_REMOTE_SERVER_MODE", "False")
     return run_in_remote_server_mode.lower() == "true"
